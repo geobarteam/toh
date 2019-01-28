cc8f3cac850a8014c3e01f61b14b673caff6dc33
geobarteam <gvd8@hotmail.com>
2019-01-28 17:35:40 +0100

    only exception page in development env

--- toh-webapi/Startup.cs
+++ toh-webapi/Startup.cs
@@ -39,7 +39,11 @@ namespace toh_webapi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseMvc();
         }
     }
